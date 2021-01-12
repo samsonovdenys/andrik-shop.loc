62eb30ade1a35392775a35f6016fc10195cf47ef
samsonovdenys <ds126@libero.it>
2021-01-12 22:57:35 +0100

    header,page-menu,search-panel done!

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,6 +1,5 @@
 @@include("burger.js");
 
-
 function testWebP(callback) {
     var webP = new Image();
     webP.onload = webP.onerror = function () {
@@ -18,27 +17,54 @@ testWebP(function (support) {
     }
 });
 
-
-
-
 //BURGER
-let menuPageBurger = document.querySelector('.menu-page__burger');
-let menuPageBody = document.querySelector('.menu-page__body');
-menuPageBurger.addEventListener("click", function (e){
-    menuPageBurger.classList.toggle('_active');
-    menuPageBody.classList.toggle('_active');
+let menuPageBurger = document.querySelector(".menu-page__burger");
+let menuPageBody = document.querySelector(".menu-page__body");
+menuPageBurger.addEventListener("click", function (e) {
+    menuPageBurger.classList.toggle("_active");
+    menuPageBody.classList.toggle("_active");
+});
+
+//MENU
+let menuParents = document.querySelectorAll(".menu-page__parent");
+for (let i = 0; i < menuParents.length; i++) {
+    let menuParent = menuParents[i];
+    menuParent.addEventListener("mouseenter", function (e) {
+        menuParent.classList.add("_active");
+    });
+    menuParent.addEventListener("mouseleave", function (e) {
+        menuParent.classList.remove("_active");
+    });
+}
+
+let searchSelect = document.querySelector('.search-page__select');
+let categoriesSearch = document.querySelector('.categories-search');
+searchSelect.addEventListener('click', function(e){
+    categoriesSearch.classList.toggle('_active');
+    searchSelect.classList.toggle('_active');
 });
 
 
+let checkboxCategories = document.querySelectorAll('.categories-search__checkbox');
+let checkedCategories=[];
+function checkElements(el){
+    return el.checked;
+}
+for(let i=0; i<checkboxCategories.length; i++){   
 
-//MENU
-let menuParents = document.querySelectorAll('.menu-page__parent');
-for (let i = 0; i<menuParents.length; i++){
-    let menuParent = menuParents[i];
-        menuParent.addEventListener("mouseenter", function (e) {
-        menuParent.classList.add('_active');
-    });
-        menuParent.addEventListener("mouseleave", function (e){
-        menuParent.classList.remove('_active');
-    });
+    let checkboxCategory = checkboxCategories[i];
+        checkboxCategory.addEventListener('click', function(e){
+
+            let checkboxes = document.querySelectorAll('.checkbox__input');
+            let checkboxesArr = Array.from(checkboxes);
+            checkedCategories = checkboxesArr.filter(checkElements);
+
+            if(checkedCategories.length > 0){
+                searchSelect.classList.add('_categories');
+                let searchQuantity = searchSelect.querySelector('.search-page__quantity');
+                searchQuantity.innerHTML = searchQuantity.getAttribute('data-text')+' '+checkedCategories.length;
+            }else{
+                searchSelect.classList.remove('_categories');
+            }
+        })
 };
